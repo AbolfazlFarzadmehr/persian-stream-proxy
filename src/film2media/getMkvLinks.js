35a02c3a6d76bb93ea13259dfcb6db8f44a60312
Abolfazl Farzadmehr <abolfazlfarzadmehr@gmail.com>
2025-08-02 07:33:21 +0330

    remover mesuring size for film2media links in proxy

--- src/film2media/getMkvLinks.js
+++ src/film2media/getMkvLinks.js
@@ -20,9 +20,9 @@ export default async function getMkvLinks(pageAddress, type, season, episode) {
       }
     });
     const trailer = $('video').first().attr('src');
-    const sizeAdded = await getSizeOfArrLinks(mkvLinks);
-    if (trailer) sizeAdded.push({ url: trailer });
-    return sizeAdded;
+    // const sizeAdded = await getSizeOfArrLinks(mkvLinks);
+    if (trailer) mkvLinks.push({ url: trailer });
+    return mkvLinks;
   } catch (err) {
     console.error(
       `Failed to get links from Page address in film2media: ${err.message}`
